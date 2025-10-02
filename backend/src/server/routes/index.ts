1185092b2ae162b5e8f8b638f44dac15b5981769
= <akhilmhdh@gmail.com>
2025-10-02 22:55:12 +0530

    feat: battle of ts - unlimited edition

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -1243,7 +1243,12 @@ export const registerRoutes = async (
     projectTemplateService,
     smtpService,
     reminderService,
-    notificationService
+    notificationService,
+    membershipGroupDAL,
+    membershipIdentityDAL,
+    membershipRoleDAL,
+    membershipUserDAL,
+    roleDAL
   });
 
   const projectEnvService = projectEnvServiceFactory({
